bbd74d9dc3da5da34a47f60b9a6885917664cb83
Shamil Arslanov <66618116+homomorfism@users.noreply.github.com>
2021-11-08 10:22:46 +0300

    Create MainClass.scala

---
+++ ScalaWordCount/src/main/scala/MainClass.scala
@@ -0,0 +1,20 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+object MainClass{
+    def main(args: Array[String]){
+
+        val conf = new SparkConf().setAppName("appName")
+        val sc = new SparkContext(conf)
+
+        val inputPath = args(0)
+        val outputPath = args(1)
+        
+        val textFile = sc.textFile(inputPath)
+        val counts = textFile.flatMap(line => line.split(" "))
+                         .map(word => (word, 1))
+                         .reduceByKey(_ + _)
+        counts.saveAsTextFile(outputPath)
+    }
+}
+r
